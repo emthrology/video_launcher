a775762dc9db00e3edc6ac18dc0a95e8b370cd97
Dongmin <emthrology@gmail.com>
2024-03-11 18:00:49 +0900

    video 실행을 위한 state 만들기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,49 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
+  XFile? video;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-          width: MediaQuery.of(context).size.width,
-          // color: Colors.black,
-          decoration: getBoxDecoration(),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              _Logo(),
-              SizedBox(height: 30.0,),
-              _AppName()
-            ],
-          ),
-        )
+        body: video == null ? renderEmpty() : renderVideo()
     );
+  }
+
+  Widget renderVideo() {
+    return Center(
+      child: Text('Video'),
+    );
+  }
+
+  Widget renderEmpty() {
+    return Container(
+      width: MediaQuery
+          .of(context)
+          .size
+          .width,
+      // color: Colors.black,
+      decoration: getBoxDecoration(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _Logo(
+            onTap: onLogoTap,
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          _AppName()
+        ],
+      ),
+    );
+  }
+
+  void onLogoTap() async {
+    final video = await ImagePicker().pickVideo(
+      source: ImageSource.gallery,
+    );
+
+    if (video != null) {
+      setState(() {
+        this.video = video;
+      });
+    }
   }
 
   BoxDecoration getBoxDecoration() {
     return BoxDecoration(
         gradient: LinearGradient(
-        begin: Alignment.topCenter,
-        end: Alignment.bottomCenter,
-        colors: [
-        Color(0xff2A3A7C),
-        Color(0xff000118)
-        ]
-      )
-    );
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xff2A3A7C), Color(0xff000118)]));
   }
 }
 
 class _Logo extends StatelessWidget {
-  const _Logo({super.key});
+  final VoidCallback onTap;
+
+  const _Logo({required this.onTap, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset(
-        'asset/image/logo.png'
+    return GestureDetector(
+      onTap: onTap,
+      child: Image.asset('asset/image/logo.png'),
     );
   }
 }
@@ -54,23 +88,12 @@ class _AppName extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final textStyle = TextStyle(
-        color: Colors.white,
-        fontSize: 30.0,
-        fontWeight: FontWeight.w300
-    );
+        color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.w300);
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text(
-            'VIDEO',
-            style: textStyle
-        ),
-        Text(
-            'PLAYER',
-            style: textStyle.copyWith(
-                fontWeight: FontWeight.w700
-            )
-        )
+        Text('VIDEO', style: textStyle),
+        Text('PLAYER', style: textStyle.copyWith(fontWeight: FontWeight.w700))
       ],
     );
   }
