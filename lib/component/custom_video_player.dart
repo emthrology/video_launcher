673eb021307945e5463adead813e778f43502571
Dongmin <emthrology@gmail.com>
2024-03-18 18:55:54 +0900

    vc 슬라이더(동영상 아래 네비 바) 구현

--- lib/component/custom_video_player.dart
+++ lib/component/custom_video_player.dart
@@ -15,6 +15,7 @@ class CsutomVideoPlayer extends StatefulWidget {
 
 class _CsutomVideoPlayerState extends State<CsutomVideoPlayer> {
   VideoPlayerController? vc;
+  Duration currentPosition = Duration();
 
   @override
   void initState() {
@@ -30,6 +31,13 @@ class _CsutomVideoPlayerState extends State<CsutomVideoPlayer> {
         );
 
     await vc!.initialize();
+
+    vc!.addListener(() {
+      final currentPosition = vc!.value.position;
+      setState(() {
+        this.currentPosition = currentPosition;
+      });
+    });
 
     setState(() {});
   }
@@ -51,53 +59,63 @@ class _CsutomVideoPlayerState extends State<CsutomVideoPlayer> {
           onFFPressed: onFFPressed,
           isPlaying: vc!.value.isPlaying,
         ),
-        Positioned(
-          //absolute-like
-          right: 0,
-          child: IconButton(
-              onPressed: () {},
-              color: Colors.white,
-              iconSize: 30.0,
-              icon: Icon(
-                Icons.photo_camera_back,
-              )),
-        )
+        _NewVideo(
+          onPressed: onNewVideoPressed,
+        ),
+        _SliderBottom(
+          currentPosition: currentPosition,
+          maxPosition: vc!.value.duration,
+          onSliderChanged: onSliderChanaged,
+        ),
       ]),
     );
   }
 
+  void onSliderChanaged(double val) {
+    vc!.seekTo(
+      Duration(
+        seconds: val.toInt()
+      )
+    );
+  }
+
+
+  void onNewVideoPressed() {}
+
   void onPlayPressed() {
     //toggle play state
-    setState(() { //like watching
-      if(vc!.value.isPlaying) {
+    setState(() {
+      //like watching
+      if (vc!.value.isPlaying) {
         vc!.pause();
-      } else{
+      } else {
         vc!.play();
       }
     });
-
   }
+
   void onRewindPressed() {
     final jump = Duration(milliseconds: 3000);
     final currentPosition = vc!.value.position;
     final minPosition = Duration.zero;
     Duration position;
-    if(currentPosition < jump) {
+    if (currentPosition < jump) {
       position = minPosition;
-    }else {
+    } else {
       position = currentPosition - jump;
     }
     vc!.seekTo(position);
   }
+
   void onFFPressed() {
     final jump = Duration(milliseconds: 3000);
     final currentPosition = vc!.value.position;
     final maxPosition = vc!.value.duration;
     final timeLeft = maxPosition - currentPosition;
     Duration position;
-    if(timeLeft < jump) {
+    if (timeLeft < jump) {
       position = maxPosition;
-    }else {
+    } else {
       position = currentPosition + jump;
     }
     vc!.seekTo(position);
@@ -150,6 +168,76 @@ class _Controls extends StatelessWidget {
       iconSize: 30.0,
       color: Colors.white,
       icon: Icon(iconData),
+    );
+  }
+}
+
+class _NewVideo extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const _NewVideo({required this.onPressed, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      //absolute-like
+      right: 0,
+      child: IconButton(
+          onPressed: onPressed,
+          color: Colors.white,
+          iconSize: 30.0,
+          icon: Icon(
+            Icons.photo_camera_back,
+          )),
+    );
+  }
+}
+
+class _SliderBottom extends StatelessWidget {
+  final Duration currentPosition;
+  final Duration maxPosition;
+  final ValueChanged<double> onSliderChanged;
+
+  const _SliderBottom({
+    required this.currentPosition,
+    required this.maxPosition,
+    required this.onSliderChanged,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 0,
+      right: 0,
+      left: 0,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            Text(
+              '${(currentPosition.inMinutes % 60).toString().padLeft(2, '0')}:${(currentPosition.inSeconds % 60).toString().padLeft(2, '0')}',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            Expanded(
+              child: Slider(
+                value: currentPosition.inSeconds.toDouble(),
+                onChanged: onSliderChanged,
+                max: maxPosition.inSeconds.toDouble(),
+                min: 0,
+              ),
+            ),
+            Text(
+              '${(maxPosition.inMinutes % 60).toString().padLeft(2, '0')}:${(maxPosition.inSeconds % 60).toString().padLeft(2, '0')}',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
